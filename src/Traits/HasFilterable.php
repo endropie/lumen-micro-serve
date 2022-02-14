a4aa4d987e3c4e1efd66b76479f3e8709a5e6b4d
endropie <hendrowibo@msn.com>
2022-02-15 00:59:32 +0700

    feat: HasFilterable

---
+++ src/Traits/HasFilterable.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Endropie\LumenMicroServe\Traits;
+
+use Endropie\ApiToolkit\Http\Filter;
+
+trait HasFilterable
+{
+
+    public function scopeFilter($query, Filter $filter = null)
+    {
+        if (!$filter) $filter = new Filter;
+
+        return $filter->apply($query);
+    }
+
+    public function scopeCollective($query, $limit = 10)
+    {
+        $limit = request()->get('limit', $limit);
+
+        return request()->has('with-limitation')
+            ? $query->limitation()
+            : $query->pagination();
+    }
+
+    public function scopePagination($query, $limit = 10)
+    {
+        $limit = request()->get('limit', $limit);
+
+        if ($limit == '*') $limit = $query->count();
+
+        return $query->paginate($limit);
+    }
+
+    public function scopeLimitation($query, $limit = 10)
+    {
+        $limit = request()->get('limit', $limit);
+        $offset = request()->get('offset', 0);
+
+        return ($limit == '*') ? $query->get()
+            : $query->limit($limit)->offset($offset)->get();
+    }
+}
